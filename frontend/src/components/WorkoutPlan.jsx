b275671ea05e726ac944615cdad5bf6543b8cb43
unknown <mossysm96@gmail.com>
2023-08-11 06:34:14 -0500

    trying to break out components... Need to branch out to save current working version

--- frontend/src/components/WorkoutPlan.jsx
+++ frontend/src/components/WorkoutPlan.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import WorkoutPlan from './Workout';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import WorkoutPlan from './Workout';
 
-<div>
-    <h2>Workout Plan:</h2>
-    <WorkoutPlan />
-</div>
+
+// <div>
+//     <h2>Workout Plan:</h2>
+//     <WorkoutPlan />
+// </div>
