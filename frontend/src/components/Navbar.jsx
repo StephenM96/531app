d8bfcfa68706c38f409fe6f889e3b5ff9b11da83
unknown <mossysm96@gmail.com>
2023-08-26 11:21:40 -0500

    completed login

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const isAuthenticated = sessionStorage.getItem("authenticated") || false;
+
   return (
     <nav>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link to="/workout-plan">Make a plan!</Link>
-        </li>
-        <li>
-          <Link to="/archive">Workout Archive</Link>
-        </li>
+        {isAuthenticated ? (
+          <>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/workout-plan">Make a plan!</Link>
+            </li>
+            <li>
+              <Link to="/archive">Workout Archive</Link>
+            </li>
+          </>
+        ) : null}
       </ul>
     </nav>
   );
