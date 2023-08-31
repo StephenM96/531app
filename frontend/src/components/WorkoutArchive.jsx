0cac79069ab0688dd3475e038bda2093551645e1
unknown <mossysm96@gmail.com>
2023-08-30 20:40:01 -0500

    need to finish out save workout component

--- frontend/src/components/WorkoutArchive.jsx
+++ frontend/src/components/WorkoutArchive.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
+import "./WorkoutArchiveStyle.css"
 
 const WorkoutArchive = ({archivedWorkouts, setArchivedWorkouts}) => {
 console.log("Archive: ", archivedWorkouts)
     return (
-        <div>
+        <div className="workout-container">
             <h2>Archived Workouts</h2>
             <ul>
                 {archivedWorkouts.map((workout, index) => (
                     <li key={index}>
                         {/* display archived workout here */}
                         {/* Start Date: {workout.startDate.toDateString()},  */}
-                        Lift: {workout.lift}
+                        Lift: {workout.lifts[0]}
                     </li>
                 ))}
             </ul>
